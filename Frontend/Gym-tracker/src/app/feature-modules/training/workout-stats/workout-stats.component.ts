1f916ef9a7b2ad0e5f316530fc0c7d6ed7fe990b
Strahinja Banjanac <strahinja.banjanac@gmail.com>
2025-07-24 13:38:10 +0200

    Fix: Headers for every page

--- Frontend/Gym-tracker/src/app/feature-modules/training/workout-stats/workout-stats.component.ts
+++ Frontend/Gym-tracker/src/app/feature-modules/training/workout-stats/workout-stats.component.ts
@@ -11,6 +11,21 @@ import { WeeklyWorkoutStats } from '../model/weekly-workout-stats';
 export class WorkoutStatsComponent {
   statsForm: FormGroup;
   weeklyStats: WeeklyWorkoutStats[] = [];
+  months = [
+  { value: 1, label: 'January' },
+  { value: 2, label: 'February' },
+  { value: 3, label: 'March' },
+  { value: 4, label: 'April' },
+  { value: 5, label: 'May' },
+  { value: 6, label: 'June' },
+  { value: 7, label: 'July' },
+  { value: 8, label: 'August' },
+  { value: 9, label: 'September' },
+  { value: 10, label: 'October' },
+  { value: 11, label: 'November' },
+  { value: 12, label: 'December' }
+];
+
 
   constructor(private fb: FormBuilder, private trainingService: TrainingService) {
     this.statsForm = this.fb.group({
